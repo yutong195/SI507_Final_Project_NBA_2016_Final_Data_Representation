2caf81ee28549a6a56902e3cc179ffd80a823c0c
yutong195 <96518793+yutong195@users.noreply.github.com>
2023-12-09 21:57:44 -0500

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -71,7 +71,7 @@ def get_team_data(url, desired_table_index):
     
     else:
         print(f"Failed to retrieve content, status code: {response.status_code}")
-'''
+
 #Game 1 data caching process
 #Caching the g1 warriors data
 g1_war_player = get_player_data("https://www.basketball-reference.com/boxscores/201606020GSW.html",8)
@@ -111,7 +111,7 @@ g2_cav_team.to_csv('data/g2_cav_team_data.csv', index=False)
 g2_war_team = get_team_data("https://www.basketball-reference.com/boxscores/shot-chart/201606050GSW.html",1)
 print(g2_war_team)
 g2_war_team.to_csv('data/g2_war_team_data.csv', index=False)
-'''
+
 
 #Caching game3 data
 #Caching the g3 warriors data
@@ -148,9 +148,8 @@ g4_cav_team.to_csv('data/g4_cav_team_data.csv', index=False)
 #Caching the g4 warriors team data
 g4_war_team = get_team_data("https://www.basketball-reference.com/boxscores/shot-chart/201606100CLE.html",0)
 g4_war_team.to_csv('data/g4_war_team_data.csv', index=False)
-'''
-'''
-'''
+
+
 #Caching game5 data
 #Caching the g5 warriors data
 g5_war_player = get_player_data("https://www.basketball-reference.com/boxscores/201606130GSW.html",8)
@@ -167,7 +166,6 @@ g5_cav_team.to_csv('data/g5_cav_team_data.csv', index=False)
 #Caching the g5 warriors team data
 g5_war_team = get_team_data("https://www.basketball-reference.com/boxscores/shot-chart/201606130GSW.html",1)
 g5_war_team.to_csv('data/g5_war_team_data.csv', index=False)
-'''
 
 
 #Caching game6 data
@@ -188,7 +186,7 @@ g6_war_team = get_team_data("https://www.basketball-reference.com/boxscores/shot
 g6_war_team.to_csv('data/g6_war_team_data.csv', index=False)
 
 
-'''
+
 #Caching game7 data
 #Caching the g7 warriors data
 g7_war_player = get_player_data("https://www.basketball-reference.com/boxscores/201606190GSW.html",8)
@@ -205,27 +203,4 @@ g7_cav_team.to_csv('data/g7_cav_team_data.csv', index=False)
 #Caching the g7 warriors team data
 g7_war_team = get_team_data("https://www.basketball-reference.com/boxscores/shot-chart/201606190GSW.html",1)
 g7_war_team.to_csv('data/g7_war_team_data.csv', index=False)
-'''
 
-
-'''
-response = requests.get("https://www.basketball-reference.com/boxscores/shot-chart/201606020GSW.html")
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Try to find all tables on the page
-    tables = soup.find_all('table')
-
-    # Loop through all found tables and print out their summaries or a small part of them
-    # This is to help identify the table you're interested in
-    for i, table in enumerate(tables):
-        # Convert the table to a dataframe
-        df = pd.read_html(str(table))[0]
-        # Print out the first few rows of the dataframe
-        print(f"Table {i}:")
-        print(df.head(10))
-        print("\n")
-'''
